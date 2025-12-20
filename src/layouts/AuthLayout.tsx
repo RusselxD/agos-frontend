7472811de3452ccbd3ed7455937569afec11c0ae
Russel Cabigquez <russelcabigquez8@gmail.com>
2025-12-20 18:24:58 +0800

    a

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 
 export default function AuthLayout(): React.JSX.Element {
     return (
-        <div className="flex min-h-screen min-w-screen">
+        <div className="flex min-h-screen min-w-screen bg-white">
             <DesktopLoginFrame />
             <Outlet />
         </div>
