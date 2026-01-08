347a09a3d6a6a1884c1c110f181edebbd095f9a3
Russel Cabigquez <russelcabigquez8@gmail.com>
2026-01-09 01:06:41 +0800

    a

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,7 +1,19 @@
+import { useEffect } from "react";
 import AuthFormContainer from "../../components/common/auth/AuthFormContainer";
+import { useAuth } from "../../context/AuthContext";
 import LoginForm from "./components/LoginForm";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
+    const { isAuthenticated } = useAuth();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (isAuthenticated) {
+            navigate("/");
+        }
+    }, [isAuthenticated, navigate]);
+
     return (
         <AuthFormContainer
             title="LOGIN"
