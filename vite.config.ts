38d94e9b67011c70e5211780d43a3faf7e75956e
RusselxD <russelcabigquez8@gmail.com>
2026-02-19 22:22:12 +0800

    made this a pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,52 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    react: ["react", "react-dom", "react-router-dom"],
+                    charts: ["chart.js", "react-chartjs-2"],
+                    excel: ["exceljs", "xlsx"],
+                    utils: [
+                        "axios",
+                        "date-fns",
+                        "papaparse",
+                        "react-toastify",
+                        "lucide-react",
+                    ],
+                    hls: ["hls.js"],
+                },
+            },
+        },
+    },
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: "autoUpdate",
+            devOptions: {
+                enabled: true,
+            },
+            workbox: {
+                maximumFileSizeToCacheInBytes: 3 * 1024 * 1024, // 3 MiB
+                globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+            },
+            manifest: {
+                name: "AGOS",
+                short_name: "AGOS",
+                display: "standalone",
+                background_color: "#ffffff",
+                theme_color: "#ffffff",
+                icons: [
+                    {
+                        src: "/agos.png",
+                        sizes: "1000x1000",
+                        type: "image/png",
+                    },
+                ],
+            },
+        }),
+    ],
+});
