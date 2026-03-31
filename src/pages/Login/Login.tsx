676a80a0c942168b1c9c2870499857cccd085fec
RusselxD <russelcabigquez8@gmail.com>
2026-03-31 09:11:33 +0800

    fix login redirection

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -10,7 +10,7 @@ export default function Login() {
 
     useEffect(() => {
         if (isAuthenticated) {
-            navigate("/");
+            navigate("/admin");
         }
     }, [isAuthenticated, navigate]);
 
